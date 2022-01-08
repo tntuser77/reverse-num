d333cc6a27d18636d020ec981fb94d0cbf89e903
Elijah Shope <eshope7489@gmail.com>
2022-01-08 13:55:48 -0500

    First Commit

---
+++ reverse num.py
@@ -0,0 +1,11 @@
+class reverse:
+    def reverse(x):
+
+        y = x
+
+        z = 0
+        while x > 0:
+            a = x % 10
+            z = z * 10 + a
+            x = x // 10
+        return z
